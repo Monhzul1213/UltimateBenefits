dd91b688d230438626a6b49e6a936bb6c7182d0e
Battugs Bilguun <145326133+rimzss@users.noreply.github.com>
2024-07-18 09:22:11 +0800

    42 table fix (#43)
    * table filter added
    
    * table fixed

--- src/lib/actions/learning.actions.js
+++ src/lib/actions/learning.actions.js
@@ -1,13 +1,25 @@
 import myAxios from "../axios";
+import { alert } from "./alert.actions";
 export const getLearningData = async (UserID) => {
   const userToken = sessionStorage.getItem("userToken");
   try {
-    const { data } = await myAxios.get("/api/learning/getLearning", {
+    const { data } = await myAxios.get("/api/training", {
       headers: {
         Authorization: `Bearer ${userToken}`,
       },
     });
     return data.result;
+  } catch (error) {
+    if (!error.response) {
+      alert("Уучлаарай, сүлжээ унасан байна", "error");
+    } else {
+      alert(error.response.data.error.message, "error");
+    }
+  }
+};
+
+export const addLearningData = async () => {
+  try {
   } catch (error) {
     if (!error.response) {
       alert("Уучлаарай, сүлжээ унасан байна", "error");
