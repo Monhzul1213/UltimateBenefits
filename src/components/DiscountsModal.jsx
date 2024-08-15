f40e3da76e25b26cded1ba2130de2f5a92a95173
Narmandakh Udwal <116719076+uchkaan96@users.noreply.github.com>
2024-08-15 12:08:38 +0800

    80 change discounts (#100)
    * temporar3
    
    * temporar4
    
    * temporar5
    
    * temporar5

--- src/components/DiscountsModal.jsx
+++ src/components/DiscountsModal.jsx
@@ -1,72 +1,70 @@
 import React, { useState } from 'react';
-import Modal from 'react-modal';
+import { Modal } from 'antd';
 import { FaXmark } from "react-icons/fa6";
 import { useDiscounts } from "../context/DiscountsProvider";
-import { tuya, bataa } from '../assets';
+//import { tuya, bataa } from '../assets';
 
 const DiscountsModal = ({ isOpen, onRequestClose, discount }) => {
-  const [contextMenu, setContextMenu] = useState({ visible: false, x: 0, y: 0 });
-  const { discountsForm } =  useDiscounts();
+  const [contextMenu, setContextMenu] = useState();
+  const { discountsForm } = useDiscounts();
 
   if (!discount) return null;
 
   const handleRightClick = (event) => {
     event.preventDefault();
-    setContextMenu({
-      visible: true,
-      x: event.clientX,
-      y: event.clientY,
-    });
+    setContextMenu();
   };
 
   const handleContextMenuOptionClick = (option) => {
-    setContextMenu({ visible: false, x: 0, y: 0 });
+    setContextMenu();
   };
 
   const handleModalClick = () => {
-    setContextMenu({ visible: false, x: 0, y: 0 });
+    setContextMenu();
   };
 
   return (
     <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
+      open={isOpen}
+      onCancel={onRequestClose}
       contentLabel="Discounts"
       className="discounts-modal-content"
-      overlayClassName="modal-overlay"
       onClick={handleModalClick}
+      width={1000}
+      footer={null}
+      closable={false} 
     >
-      <div className='discounts-modal-image'>
-      <img src={`data:image/png;base64,${discount.Image}`} alt={discount.Name} />
-      </div>
-      <section>
-        <div className="discount-modal-body">
-          <div className="discounts-detail-container">
-            <h2>{discount.Name}</h2>
-            <div onClick={onRequestClose} className="discounts-close-button">
-              <FaXmark size={30} />
+      <div className="modal-body">
+        <div className="discounts-modal-image">
+          <img src={`data:image/png;base64,${discount.Image}`} alt={discount.Name} />
+        </div>
+        <section>
+          <div className="discount-modal-body">
+            <div className="discounts-detail-container">
+              <h2>{discount.Name}</h2>
+              <div onClick={onRequestClose} className="discounts-close-button">
+                <FaXmark size={30} />
+              </div>
             </div>
-          </div>
 
-          <div className="discounts-modal-body" onContextMenu={handleRightClick}>
-          <div className="modal-section1">
-           <strong>МЭДЭЭЛЭЛ</strong>
-           <p style={{ whiteSpace: "pre-wrap" }}>{discount.Descr}</p>
-          </div>
-            
-            {discountsForm.Type === "0" ? (
-            <div className="modal-section1">
-              <strong>Шаардлагтай ажилласан хугацаа /сараар/</strong>
-              <p>{discount.AvailableCount}</p>
-            </div>
-            ) : (
-            <div className="modal-section1">
-              <strong>Хэдэн удаа</strong>
-              <p>{discount.AvailableCount}</p>
-            </div>
-            )}
+            <div className="discounts-modal-body" onContextMenu={handleRightClick}>
+              <div className="modal-section1">
+                <strong>МЭДЭЭЛЭЛ</strong>
+                <p style={{ whiteSpace: "pre-wrap" }}>{discount.Descr}</p>
+              </div>
 
-            {/* <div className="modal-section1">
+              {discountsForm.Type === "0" ? (
+                <div className="modal-section1">
+                  <strong>Шаардлагтай ажилласан хугацаа /сараар/</strong>
+                  <p>{discount.AvailableCount}</p>
+                </div>
+              ) : (
+                <div className="modal-section1">
+                  <strong>Хэдэн удаа</strong>
+                  <p>{discount.AvailableCount}</p>
+                </div>
+              )}
+              {/* <div className="modal-section1">
               <strong>ӨМНӨХ САРЫН ШИЛДЭГ АЖИЛТНУУД</strong>
               <div style={{ display: 'flex', gap: '30px', paddingTop: '10px' }}>
                 <div className="employee-photo-container">
@@ -83,9 +81,10 @@ const DiscountsModal = ({ isOpen, onRequestClose, discount }) => {
                 </div>
               </div>
             </div> */}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </Modal>
   );
 };
